d1fdfa6d39a6e7781654d0dee9a16da4ae319a7c
Wellington <wellington.cerqueira32@gmail.com>
2026-02-20 16:00:01 -0300

    Add Livro domain model

---
+++ BookWishlistAPI/Domain/Livro.cs
@@ -0,0 +1,16 @@
+﻿namespace BookWishlistAPI.Domain
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor {  get; set; }
+        public int AnoPublicacao { get; set; }
+        public string Editora { get; set;}
+        public string Genero { get; set; }
+        public decimal Preco { get; set; }
+        public string Prioridade { get; set; }
+        public DateTime DataAdicao { get; set; }
+
+    }
+}
